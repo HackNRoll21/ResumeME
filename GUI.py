06517f1b9bce1608936b9a9454a45d40f9c4fb1a
sean <sean@localhost.localdomain>
2021-01-09 03:03:20 +0800

    Add pics

--- GUI.py
+++ GUI.py
@@ -8,7 +8,7 @@ import json
 
 class ourwindow(Gtk.Window):
     def __init__(self):
-        Gtk.Window.__init__(self, title="ResumeWriter Program")
+        Gtk.Window.__init__(self, title="Resumeme")
         Gtk.Window.set_default_size(self, 400,325)
         Gtk.Window.set_position(self, Gtk.WindowPosition.CENTER)
 
